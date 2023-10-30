a8de17ec58b8b4ded72bb1e2475984ce49949a0e
Afamdcy <vafamdcygaming@gmail.com>
2023-10-30 21:19:46 +0600

    created login logout profile function

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
-
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -10,8 +10,11 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-about')
+            messages.success(request, f'{username},Your account has been created1 You can login !')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
